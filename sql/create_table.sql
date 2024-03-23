45846348d94f32681ad2282436f02f026fe37e66
Liu Zhongnuo <2313678365@qq.com>
2024-03-23 14:32:02 +0800

    initial commit

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,10 +3,10 @@
 # @from <a href="https://yupi.icu">编程导航知识星球</a>
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists my_oj;
 
 -- 切换库
-use my_db;
+use my_oj;
 
 -- 用户表
 create table if not exists user
